ba1ce99bb07f0c2124a89edd12bfdde6428c7778
mpinel2 <montse.pinel@gmail.com>
2023-05-19 21:18:41 +0200

    creacion de los componentes

---
+++ FrontTeacherApp/teacherApp/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { C404Component } from './components/c404/c404.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: "", pathMatch: 'full', redirectTo: 'home' },
+  { path: "home", component: HomeComponent },
+  { path: "**", component: C404Component }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
